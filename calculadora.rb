10765e1a78041549892893283deedb14daf00427
Rafael Neves <rafaelneves0405@gmail.com>
2025-08-18 22:16:33 -0300

    feat: calculator with operation order

--- calculadora.rb
+++ calculadora.rb
@@ -5,28 +5,36 @@ class Calculator
       whatToCalculate = calculation.split(/([+\-\/\*])/)
       index = 0
 
+      secondPass = false
+      operation = true
+
       while index < whatToCalculate.size 
         value = whatToCalculate[index]
-        
-        if(value == '+')
+        if(value == '+' and secondPass)
           whatToCalculate[index+1] = sum(whatToCalculate[index-1],whatToCalculate[index+1])
-          whatToCalculate.shift(2)
-          index -= 2
-        elsif(value == '-')
+          operation = true
+        elsif(value == '-' and secondPass)
           whatToCalculate[index+1] = subtraction(whatToCalculate[index-1],whatToCalculate[index+1])
-          whatToCalculate.shift(2)
-          index -= 2
-        elsif(value == '/')
+          operation = true
+        elsif(value == '/' and !secondPass)
           whatToCalculate[index+1] = division(whatToCalculate[index-1],whatToCalculate[index+1])
-          whatToCalculate.shift(2)
-          index -= 2
-        elsif(value == '*')
+          operation = true
+        elsif(value == '*' and !secondPass)
           whatToCalculate[index+1] = multiplication(whatToCalculate[index-1],whatToCalculate[index+1])
-          whatToCalculate.shift(2)
+          operation = true
+        else
+          operation = false
+        end
+        if operation
+          whatToCalculate.delete_at(index)
+          whatToCalculate.delete_at(index-1)
           index -= 2
         end
-
         index+=1
+        if index == whatToCalculate.size and !secondPass
+          index = 0
+          secondPass = true
+        end
       end
       
       return whatToCalculate
@@ -49,9 +57,7 @@ class Calculator
     end
   end
 
-
-
 end
 
 
-puts Calculator.calculate("4*2/2")
+puts Calculator.calculate("1+2*2/4+1")
